2c5fa09018f8521a6a4eeec58cacf587be35e50f
dhikrama creator <dhikrama.group@gmail.com>
2021-05-09 11:54:35 +0700

    dhikrama server

---
+++ app.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func main() {
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Static("/", "./views/assets")
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	log.Fatal(app.Listen(":80"))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dhikramagroup/server
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.9.0
+	github.com/gofiber/template v1.6.8
+)
